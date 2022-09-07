68cd236544453eeb2a59e9d7b06fbfeb1651d0f0
jerryjiang <jerryjiang@didichuxing.com>
2022-09-07 16:26:28 +0800

    feat: screenshot

--- screenshot.js
+++ screenshot.js
@@ -6,7 +6,7 @@ const ora = require("ora");
 const spinner = ora("Loading...");
 
 const puppeteer = require("puppeteer");
-const port = "8003";
+const port = "8002";
 
 const waitTime = (time = 100) => {
   return new Promise((resolve) => {
@@ -17,7 +17,7 @@ const waitTime = (time = 100) => {
 };
 
 let scrape = async (path) => {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: true });
   path = path.replace(/\'|\s/g, "");
   fs.readdir(path, async (err, files) => {
     if (err) {
@@ -35,8 +35,8 @@ let scrape = async (path) => {
           deviceScaleFactor: 1,
         });
         await page.goto(`http://localhost:${port}/#${dirs[i]}`);
-        let rhs = await page.$("#js-ide");
-        await waitTime(2000);
+        let rhs = await page.$("#ide-preview");
+        await waitTime(5000);
         await rhs.screenshot({
           path: `./screenshots/${dirs[i]}.png`,
         });
